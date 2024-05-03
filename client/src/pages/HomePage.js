cfd21c10dec45f29b912144ba18445ad86d2b344
hrizwan3 <hrizwan5472013@gmail.com>
2024-05-03 15:00:43 -0400

    merged

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -12,9 +12,8 @@ export default function HomePage() {
     // Fetch the car of the day, which is actually a review that includes car details
     fetch(`http://${config.server_host}:${config.server_port}/car_of_the_day`)
       .then(res => res.json())
-      .then(resJson => setSongOfTheDay(resJson));
-
-    
+      .then(resJson => setCarOfTheDay(resJson))
+      .catch(error => console.error("Failed to fetch car of the day:", error));
   }, []);
 
   // Define the columns for the car table
@@ -44,15 +43,8 @@ export default function HomePage() {
         }
       </p>
       <Divider />
-      <h2>Top Songs</h2>
-      <LazyTable route={`http://${config.server_host}:${config.server_port}/top_songs`} columns={songColumns} />
-      <Divider />
-      {/* TODO (TASK 16): add a h2 heading, LazyTable, and divider for top albums. Set the LazyTable's props for defaultPageSize to 5 and rowsPerPageOptions to [5, 10] */}
-      <h2>Top Albums</h2>
-      <LazyTable route={`http://${config.server_host}:${config.server_port}/top_albums`} columns={albumColumns} defaultPageSize={5} rowsPerPageOptions={[5,10]}/>
-      <Divider />
-      {/* TODO (TASK 17): add a paragraph (<p></p>) that displays “Created by [name]” using the name state stored from TASK 13/TASK 14 */}
-      
+      <h2>Top Cars</h2>
+      <LazyTable route={`http://${config.server_host}:${config.server_port}/top_cars`} columns={carColumns} defaultPageSize={5} rowsPerPageOptions={[5, 10, 25]} />
     </Container>
   );
 };
